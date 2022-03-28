455b20c857973556c2e786256e6c5e4026809ba3
David Jantzen <mr.djantzen@gmail.com>
2022-03-27 18:05:04 -0700

    add method for waiting a period of time

--- src/singletons/clocktower.py
+++ src/singletons/clocktower.py
@@ -2,7 +2,7 @@
 The ClockTower is the single source of truth for time in the system. This enhances testability by allowing
 test suites to set values for current time and sleep durations
 """
-
+import _thread
 import utime
 
 
@@ -13,6 +13,7 @@ class ClockTower:
 
     def __init__(self):
         self.sleep_duration = None
+        self.not_yets = {}
 
     @classmethod
     def instance(cls):
@@ -37,3 +38,15 @@ class ClockTower:
             utime.sleep(duration_in_seconds)
         else:
             utime.sleep(self.sleep_duration)
+
+    def not_yet(self, duration_in_seconds):
+        calling_thread = _thread.get_ident()
+        if calling_thread not in self.not_yets:
+            self.not_yets[calling_thread] = self.now
+            return True
+        else:
+            if self.now >= self.not_yets[calling_thread] + duration_in_seconds:
+                del(self.not_yets[calling_thread])
+                return False
+            else:
+                return True
